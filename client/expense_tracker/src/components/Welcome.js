9c4ae6c6d375cf021b8844d2bf9bb6f5733ac256
ravar3993 <contactrahul39@gmail.com>
2021-01-17 15:38:30 +0530

    login page ui changes with antd package

--- client/expense_tracker/src/components/Welcome.js
+++ client/expense_tracker/src/components/Welcome.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import LoginPage from './LoginPage'
+import LoginForm from './LoginForm'
+import RegisterForm from './RegisterForm'
 import '../styles/welcome.css'
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
 
 class Welcome extends Component {
     constructor(){
         super()
+        this.state = {
+            isLogin: true
+        }
     }
+
+    loginClickHandler = () => {
+        if(!this.state.isLogin){
+            this.setState({
+                isLogin: true
+            })
+        }
+    }
+
+    registerClickHandler = () => {
+        if(this.state.isLogin){
+            this.setState({
+                isLogin: false
+            })
+        }
+    }
+
     render(){
+        let comp;
+        if(this.state.isLogin){
+            comp = <LoginForm />
+        }else{
+            comp = <RegisterForm />
+        }
         return (
-            <>
-            <LoginPage />
-            </>
+            <div class="login-register">
+                <div class="login-register-button">
+                <span><Button type="primary" onClick={this.loginClickHandler}>Login</Button></span>
+                <span><Button type="primary" onClick={this.registerClickHandler}>Register</Button></span>
+                </div>
+                <div class="login-register-form">{comp}</div>
+            </div>
         )
     }
 }
